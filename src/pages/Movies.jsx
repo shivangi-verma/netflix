2beb7ef306e3a0ceb2b3cbebd18d6996777747f5
Shivangi <81459301+shivangi-verma@users.noreply.github.com>
2024-03-07 12:56:41 +0530

    Updated!

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,75 @@
+// import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
+// import Navbar from "../components/Navbar";
+// // import CardSlider from "../components/CardSlider";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { firebaseAuth } from "../utils/firebase-config";
+// import { useNavigate } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
+// import { fetchMovies, getGenres } from "../store";
+// import SelectGenre from "../components/SelectGenre.jsx";
+// import Slider from "../components/Slider";
+// import NotAvailable from "../components/NotAvailable";
+
+// function MoviePage() {
+//   const [isScrolled, setIsScrolled] = useState(false);
+//   const movies = useSelector((state) => state.netflix.movies);
+//   const genres = useSelector((state) => state.netflix.genres);
+//   const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(getGenres());
+//   });
+
+//   useEffect(() => {
+//     if (genresLoaded) {
+//       dispatch(fetchMovies({ genres, type: "movie" }));
+//     }
+//   }, [genresLoaded]);
+
+//   const [user, setUser] = useState(undefined);
+
+//   onAuthStateChanged(firebaseAuth, (currentUser) => {
+//     if (currentUser) setUser(currentUser.uid);
+//     else navigate("/login");
+//   });
+
+//   window.onscroll = () => {
+//     setIsScrolled(window.pageYOffset === 0 ? false : true);
+//     return () => (window.onscroll = null);
+//   };
+
+//   return (
+//     <Container>
+//       <div className="navbar">
+//         <Navbar isScrolled={isScrolled} />
+//       </div>
+//       <div className="data">
+//         <SelectGenre genres={genres} type="movie" />
+//         {movies.length ? <Slider movies={movies} /> : <NotAvailable />}
+//       </div>
+//     </Container>
+//   );
+// }
+
+// const Container = styled.div`
+//   .data {
+//     margin-top: 8rem;
+//     .not-available {
+//       text-align: center;
+//       color: white;
+//       margin-top: 4rem;
+//     }
+//   }
+// `;
+// export default MoviePage;
+
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
-// import CardSlider from "../components/CardSlider";
 import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
 import { useNavigate } from "react-router-dom";
@@ -22,25 +90,34 @@ function MoviePage() {
 
   useEffect(() => {
     dispatch(getGenres());
-  });
+  }, [dispatch]); // Added dispatch to the dependency array
 
   useEffect(() => {
     if (genresLoaded) {
       dispatch(fetchMovies({ genres, type: "movie" }));
     }
-  }, [genresLoaded]);
+  }, [dispatch, genres, genresLoaded]); // Added dependencies
 
-  const [user, setUser] = useState(undefined);
+  // Commenting out unused user state
+  // const [user, setUser] = useState(undefined);
 
-  onAuthStateChanged(firebaseAuth, (currentUser) => {
-    if (currentUser) setUser(currentUser.uid);
-    else navigate("/login");
-  });
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (currentUser) => {
+      if (currentUser) {
+        // setUser(currentUser.uid);
+      } else {
+        navigate("/login");
+      }
+    });
+  }, [navigate]);
 
-  window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true);
-    return () => (window.onscroll = null);
-  };
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.pageYOffset === 0 ? false : true);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <Container>
--- src/pages/TVShows.jsx
+++ src/pages/TVShows.jsx
@@ -1,7 +1,83 @@
+// import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
+// import Navbar from "../components/Navbar";
+// import CardSlider from "../components/CardSlider";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { firebaseAuth } from "../utils/firebase-config";
+// import { useNavigate } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
+// import { fetchMovies, getGenres } from "../store";
+// import SelectGenre from "../components/SelectGenre.jsx";
+// import Slider from "../components/Slider";
+
+// function TVShows() {
+//   const [isScrolled, setIsScrolled] = useState(false);
+//   const movies = useSelector((state) => state.netflix.movies);
+//   const genres = useSelector((state) => state.netflix.genres);
+//   const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+//   const dataLoading = useSelector((state) => state.netflix.dataLoading);
+
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     if (!genres.length) dispatch(getGenres());
+//   }, []);
+
+//   useEffect(() => {
+//     if (genresLoaded) {
+//       dispatch(fetchMovies({ genres, type: "tv" }));
+//     }
+//   }, [genresLoaded]);
+
+//   const [user, setUser] = useState(undefined);
+
+//   onAuthStateChanged(firebaseAuth, (currentUser) => {
+//     if (currentUser) setUser(currentUser.uid);
+//     else navigate("/login");
+//   });
+
+//   window.onscroll = () => {
+//     setIsScrolled(window.pageYOffset === 0 ? false : true);
+//     return () => (window.onscroll = null);
+//   };
+
+//   return (
+//     <Container>
+//       <Navbar isScrolled={isScrolled} />
+//       <div className="data">
+//         <SelectGenre genres={genres} type="tv" />
+//         {movies.length ? (
+//           <>
+//             <Slider movies={movies} />
+//           </>
+//         ) : (
+//           <h1 className="not-available">
+//             No TV Shows avaialble for the selected genre. Please select a
+//             different genre.
+//           </h1>
+//         )}
+//       </div>
+//     </Container>
+//   );
+// }
+
+// const Container = styled.div`
+//   .data {
+//     margin-top: 8rem;
+//     .not-available {
+//       text-align: center;
+//       margin-top: 4rem;
+//     }
+//   }
+// `;
+// export default TVShows;
+
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
-import CardSlider from "../components/CardSlider";
+// Importing CardSlider but not using it
+// import CardSlider from "../components/CardSlider";
 import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
 import { useNavigate } from "react-router-dom";
@@ -15,32 +91,42 @@ function TVShows() {
   const movies = useSelector((state) => state.netflix.movies);
   const genres = useSelector((state) => state.netflix.genres);
   const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
-  const dataLoading = useSelector((state) => state.netflix.dataLoading);
+  // Unused variable dataLoading
+  // const dataLoading = useSelector((state) => state.netflix.dataLoading);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (!genres.length) dispatch(getGenres());
-  }, []);
+  }, [dispatch, genres]); // Added dispatch and genres to the dependency array
 
   useEffect(() => {
     if (genresLoaded) {
       dispatch(fetchMovies({ genres, type: "tv" }));
     }
-  }, [genresLoaded]);
+  }, [dispatch, genres, genresLoaded]); // Added dependencies
 
-  const [user, setUser] = useState(undefined);
+  // Commenting out unused user state
+  // const [user, setUser] = useState(undefined);
 
-  onAuthStateChanged(firebaseAuth, (currentUser) => {
-    if (currentUser) setUser(currentUser.uid);
-    else navigate("/login");
-  });
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (currentUser) => {
+      if (currentUser) {
+        // setUser(currentUser.uid);
+      } else {
+        navigate("/login");
+      }
+    });
+  }, [navigate]);
 
-  window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true);
-    return () => (window.onscroll = null);
-  };
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.pageYOffset === 0 ? false : true);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <Container>
@@ -53,7 +139,7 @@ function TVShows() {
           </>
         ) : (
           <h1 className="not-available">
-            No TV Shows avaialble for the selected genre. Please select a
+            No TV Shows available for the selected genre. Please select a
             different genre.
           </h1>
         )}
